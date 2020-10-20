67f9ea86d27ba202166f59043120d7cc49952d23
Andrew Stuntz <andrewbstuntz@gmail.com>
2020-10-20 09:26:19 -0700

    Check for validity by state, also returns requirements by state
    This includes a small refactor that eliminates some duplication, but more importantly adds two new functions.
    
    The first 'validateForState' a boolean if the dl is valid for a specific state/states, this provides a bit of an alternative interface to this package.
    
    Then I added 'requirementsByState' that returns the dl requirements for a specific state, this way we can return validation messages that let folks more easily fix drivers license inputs.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { CountryFormats, ValidateOptions, ValidationMatch } from './interfaces';
+import { CountryFormats, ValidateOptions, ValidationMatch, DriverLicenseFormat } from './interfaces';
 import { CA_DL } from './regex/ca-dl';
 import { US_DL } from './regex/us-dl';
 
@@ -8,26 +8,10 @@ import { US_DL } from './regex/us-dl';
  * @param dl Driver license number.
  * @param options Optional configuration options.
  */
-export function validate(dl: string, options: ValidateOptions = {}) {
+export function validate(dl: string, options: ValidateOptions = {}): ValidationMatch[] | [] | null {
   const results: ValidationMatch[] = [];
-  let formats: CountryFormats;
-  let states: string[];
-
-  switch (options.country) {
-    case 'CA':
-      formats = CA_DL;
-      break;
-    case 'US':
-    default:
-      formats = US_DL;
-      break;
-  }
-
-  if (!options.states) {
-    states = Object.keys(formats);
-  } else if (!Array.isArray(options.states)) {
-    states = [options.states];
-  }
+  const formats = countryFormats(options.country);
+  const states = statesToValidate(options.states, formats);
 
   states.forEach(state => {
     const info = formats[state];
@@ -50,4 +34,75 @@ export function validate(dl: string, options: ValidateOptions = {}) {
   });
 
   return results.length ? results : null;
+}
+
+export function validateForState(dl: string, state: string, options: ValidateOptions = {}): boolean {
+  const results: ValidationMatch[] = [];
+  const formats = countryFormats(options.country);
+
+  const validationInfo = formats[state];
+
+  if (!validationInfo) {
+    throw new Error(`Could not find state "${state}"!`);
+  }
+
+  validationInfo.forEach(({ regex, description }) => {
+    if (validateByState(dl, regex, options.ignoreCase)) {
+      results.push({
+        description,
+        state
+      });
+    }
+  });
+
+  return !!(results.length && results.length > 0);
+}
+
+export function requirementsByState(states: string | string[], options: ValidateOptions = {}) {
+  let requirements: { [key: string]: DriverLicenseFormat[] } = {};
+  if (Array.isArray(states)) {
+    states = states;
+  } else {
+    states = [states];
+  }
+
+  const formats = countryFormats(options.country);
+  states.forEach(function(state: string) {
+    if (formats[state]) {
+      requirements[state] = formats[state];
+    }
+  });
+  return requirements;
+}
+
+function countryFormats(country: string | null = null): CountryFormats {
+  let formats: CountryFormats;
+  switch (country) {
+    case 'CA':
+      formats = CA_DL;
+      break;
+    case 'US':
+    default:
+      formats = US_DL;
+      break;
+  }
+
+  return formats;
+}
+
+function statesToValidate(stateOptions: string | string[], formats: CountryFormats) {
+  let states: string[];
+
+  if (!stateOptions) {
+    states = Object.keys(formats);
+  } else if (!Array.isArray(stateOptions)) {
+    states = [stateOptions];
+  }
+
+  return states;
+}
+
+function validateByState(dl: string, regex: any, ignoreCase: boolean | null): boolean {
+  const pattern = ignoreCase ? new RegExp(regex, 'i') : regex;
+  return pattern.test(dl);
 }
